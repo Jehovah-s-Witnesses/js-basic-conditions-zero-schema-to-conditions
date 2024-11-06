e52ace255aa6b8777bab2fd45d1171cc4836c901
gribVolnushka <burgersqueen98@gmail.com>
2024-11-07 00:04:37 +0200

    changed structure of schema

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-const account  = confirm('Do you want create account?');
-let userName ;
+const isNeedAccount = confirm('Do you want create account?');
+let userName;
 let userPass;
 const noneUser = 'Guest';
+let isAuthorizedUser = false;
 
-if (account) {
+if (isNeedAccount) {
   userName = prompt('Enter your name');
-  userPass =  prompt('Enter your pass');
-  if (userName !== '' && userName !== null && userPass !== '' && userPass !== null) {
+  userPass = prompt('Enter your pass');
+  if (
+    userName !== '' && userName !== null && userPass !== '' &&
+    userPass !== null) {
     alert(`Hi,${userName}.Your acc data will be saved.`);
+    isAuthorizedUser = true;
   } else {
     alert('Your user data is incorrect. Guest mode is enabled.');
   }
@@ -15,71 +19,71 @@ if (account) {
   alert(`You are ${noneUser}`);
 }
 
-const subscription = confirm('Do you want create subscription?');
-const basicMonthSubscription = 'Basic functionality - 5$ month';
-const proMonthSubscription = 'Pro version - Basic + Personal support - 20$ month';
-const premiumMonthSubscription = 'Premium - Pro + Personal assistant - 50$ month.';
-const basicYearSubscription = 'You select 5$ in month. Only today you can create subscription on year an save 20$. Your amount was 40$.';
-const proYearSubscription = 'Your select 20$ in month.Only today you can create subscription on year an save 40$. Your amount was 200$.';
-const premiumYearSubscription = 'Your select 50$ in month.Only today you can create subscription on year an save 100$. Your amount was 500$.';
+const isNeedSubscription = confirm('Do you want create subscription?');
+const basicMonthSubscriptionNote = 'Basic functionality - 5$ month';
+const proMonthSubscriptionNote = 'Pro version - Basic + Personal support - 20$ month';
+const premiumMonthSubscriptionNote = 'Premium - Pro + Personal assistant - 50$ month.';
+const basicYearSubscriptionNote = 'You select 5$ in month. Only today you can create subscription on year an save 20$. Your amount was 40$.';
+const proYearSubscriptionNote = 'Your select 20$ in month.Only today you can create subscription on year an save 40$. Your amount was 200$.';
+const premiumYearSubscriptionNote = 'Your select 50$ in month.Only today you can create subscription on year an save 100$. Your amount was 500$.';
 const warmMessage = 'Cancelled event';
-let selectVariant;
-const registeredUser = true;
-const unknownUser = false;
+let selectedVariant;
+const userPlanBasic = 'basic';
+const userPlanPro = 'pro';
+const userPlanPremium = 'premium';
+let isSelectYearPlan;
+const result = 'Summary:';
 
-if (subscription) {
+
+if (isNeedSubscription) {
   alert('Price of subscription has different packs:');
-  alert(`1.${basicMonthSubscription} 2.${proMonthSubscription} 3.${premiumMonthSubscription}`);
-  selectVariant = prompt('Select your subscription variant');
+  alert(`1.${basicMonthSubscriptionNote} 2.${proMonthSubscriptionNote} 3.${premiumMonthSubscriptionNote}`);
+  selectedVariant = prompt('Select your subscription variant');
 
-  if (selectVariant === '1') {
-    alert(basicYearSubscription);
-  } else if (selectVariant === '2') {
-    alert(proYearSubscription);
-  } else if (selectVariant === '3') {
-    alert(premiumYearSubscription);
+  if (
+    selectedVariant !== null &&
+    selectedVariant !== '' &&
+    selectedVariant === '1'
+  ) {
+    alert(basicYearSubscriptionNote);
+  } else if ( selectedVariant !== null &&
+    selectedVariant !== '' && selectedVariant === '2') {
+    alert(proYearSubscriptionNote);
+  } else if ( selectedVariant !== null &&
+    selectedVariant !== '' && selectedVariant === '3') {
+    alert(premiumYearSubscriptionNote);
   } else {
     alert(warmMessage);
   }
 
-  const yearPlan = confirm('Do you want a year plan?');
+  if (selectedVariant !== null && selectedVariant !== '') {
+     isSelectYearPlan = confirm('Do you want a year plan?');
 
-  if (yearPlan) {
-    alert('You subscribed a year plan');
-
-    if (account === registeredUser && selectVariant === '1' ) {
-      alert(`${userName}, you selected base year subscription. Thank you`);
-    } else if (account === unknownUser && selectVariant === '1') {
-      alert(`${noneUser}, you selected base year subscription. Thank you`);
-    } else if (account === registeredUser && selectVariant === '2') {
-      alert(`${userName}, you selected pro year subscription. Thank you`);
-    } else if (account === unknownUser && selectVariant === '2') {
-      alert(`${noneUser}, you selected pro year subscription. Thank you.`);
-    } else if (account === registeredUser && selectVariant === '3') {
-      alert(`${userName}, you selected premium year subscription. Thank you`);
-    } else if (account === unknownUser && selectVariant === '3'){
-      alert(`${noneUser}, you selected premium year subscription. Thank you`);
-    }
-
-
-  } else {
-    alert('Summary');
-
-    if (account === registeredUser && selectVariant === '1') {
-      alert(`${userName}, you selected base month subscription. Thank you`);
-    } else if (account === unknownUser && selectVariant === '1') {
-      alert(`${noneUser}, you selected base month subscription. Thank you.`);
-    } else if (account === registeredUser && selectVariant === '2') {
-      alert(`${userName}, you selected pro month subscription. Thank you`);
-    } else if (account === unknownUser && selectVariant === '2') {
-      alert(`${noneUser}, you selected pro month subscription. Thank you`);
-    } else if (account === registeredUser && selectVariant === '3') {
-      alert(`${userName}, you selected premium month subscription. Thank you`);
-    } else if (account === unknownUser  && selectVariant === '3') {
-      alert(`${noneUser}, you selected premium month subscription. Thank you`);
+    if (isSelectYearPlan) {
+      alert('You subscribed a year plan');
     }
   }
 
+
+
+  if (selectedVariant === '1'
+  ) { alert(result);
+    alert(
+      `${isAuthorizedUser ? userName : noneUser}, you selected ${userPlanBasic} ${isSelectYearPlan ? 'year' : 'month'} subscription. Thank you`,
+    );
+  } else if (selectedVariant === '2') {
+    alert(result);
+    alert(
+      `${isAuthorizedUser ? userName : noneUser}, you selected ${userPlanPro} ${isSelectYearPlan ? 'year' : 'month'} subscription. Thank you`,
+    );
+  } else if (selectedVariant === '3') {
+    alert(result);
+    alert(
+      `${isAuthorizedUser ? userName : noneUser}, you selected ${userPlanPremium} ${isSelectYearPlan ? 'year' : 'month'} subscription. Thank you`,
+    );
+  }
+} else {
+  alert(warmMessage);
 }
 
 
